0f501cb6a7c72b96122385f60d6e9f3f17315d83
Unni <ksunair@yahoo.com>
2014-04-06 17:25:23 -0500

    enabled user authentication

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 ======
 
 13 favorites items (movies, music etc.,)
+
+*** User login by
+
+user id/email
+meteor add accounts-base
+meteor add accounts-password
+meteor add email
+meteor add accounts-ui
+
+others Twitter/Facebook/Google
+meteor add accounts-twitter
+meteor add accounts-google
+meteor add accounts-facebook
+
--- client/clientmain.js
+++ client/clientmain.js
@@ -1,14 +1,16 @@
-if (Meteor.isClient) {
-    Template.hello.greeting = function () {
+Template.hello.greeting = function () {
         return "Welcome to 13favs.";
-    };
+};
 
-    Template.hello.events({
+Template.hello.events({
         'click input': function () {
             // template data, if any, is available in 'this'
             if (typeof console !== 'undefined')
                 console.log("You pressed the button");
         }
-    });
-}
+});
+
+Accounts.ui.config({
+        passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+});
 
